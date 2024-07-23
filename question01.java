2fa10b319850b78e7dc47554de72f89c047674b1
Isuru23280038 <it23280038@my.sliit.lk>
2024-07-23 13:36:03 +0530

    question01_part01

---
+++ question01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class question01 
+{
+	public static void main(String[] args)
+	{
+	Scanner myobj = new Scanner(System.in);
+	
+	System.out.println("Enter your first name:");
+	String firstname = myobj.nextLine();
+	
+	System.out.println("Enter your middle name:");
+	String middlename = myobj.nextLine();
+	
+	System.out.println("Enter your last name:");
+	String lastname = myobj.nextLine();
+	
+	System.out.println("Full name:" +firstname + " " +middlename + " " +lastname);
+	
+	}
+}
